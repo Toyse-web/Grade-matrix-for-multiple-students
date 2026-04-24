5dcc4136db7cbb4202e3e6c7f1f1a26a81cb273d
Toyse-web <olayonwatoyib05@gmail.com>
2026-04-24 15:07:40 +0100

    Implement grades storage using a vector
    Added a vector to store grades for multiple students and subjects.

--- Flexible grades tracker.cpp
+++ Flexible grades tracker.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -11,6 +12,16 @@ int main() {
 	
 	cout << "How many subjects? ";
 	cin >> numSubject;
+
+	vector<vector<float>> grades(numStudent, vector<float> (numSubject));
+	
+	for (int i = 0; i < numStudent; i++) {
+		cout << "Enter student's grades:" << i + 1 << endl;
+		for (int j = 0; j < numSubject; j++) {
+			cout << " Subject " << j +  1 << " Grade ";
+			cin >> grades[i][j];
+		}
+	}
 	
 	return 0;
 }
