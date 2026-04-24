e7d5febb8ed405d330c71b3111b3006b26b51f76
Toyse-web <olayonwatoyib05@gmail.com>
2026-04-24 14:24:18 +0100

    Starting the program, takes users input.

--- Flexible grades tracker.cpp
+++ Flexible grades tracker.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+	
+	int numStudent, numSubject;
+	
+	cout << "How many students? ";
+	cin >> numStudent;
+	
+	cout << "How many subjects? ";
+	cin >> numSubject;
+	
+	return 0;
+}
