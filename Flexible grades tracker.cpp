0227b029c1604cf4f1679ed1199f96fb24b73c62
Toyse-web <olayonwatoyib05@gmail.com>
2026-04-25 12:43:01 +0100

    Implement Students and subjects total and also the average grade calculations
    Added functionality to calculate and display total and average grades for each student and subject.

--- Flexible grades tracker.cpp
+++ Flexible grades tracker.cpp
@@ -22,6 +22,30 @@ int main() {
 			cin >> grades[i][j];
 		}
 	}
+
+	cout << endl;
+	
+	for (int i = 0; i < numStudent; i++) {
+		int studentSum = 0;
+		for (int j = 0; j < numSubject; j++) {
+			studentSum += grades[i][j];
+		}
+		cout << "Student " << i + 1 << " total: "<< studentSum << endl;
+	
+	double stdAve = static_cast<double>(studentSum) / numSubject;
+	cout << "Student " << i + 1 << " average: " << fixed << setprecision(2) << stdAve << endl;
+	}
+	
+	for (int j = 0; j < numSubject; j++) {
+		int subjectSum = 0;
+		for (int i = 0; i < numStudent; i++) {
+			subjectSum += grades[i][j];
+		}
+		cout << "Subject " << j + 1 << " total: "<< subjectSum << endl;
+		
+		double sbjAve = static_cast<double>(subjectSum) / numStudent;
+	cout << "Subject " << j + 1 << " average: " << fixed << setprecision(2) << sbjAve << endl;
+	}
 	
 	return 0;
 }
